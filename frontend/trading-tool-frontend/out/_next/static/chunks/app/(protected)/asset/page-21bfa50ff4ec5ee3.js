fbb55e104d5fa49304e256b03ef23d6d0ed116d2
Gerrit <gvangalen@hotmail.com>
2026-07-18 21:38:03 +0200

    Build workspace attention previews

---
+++ frontend/trading-tool-frontend/out/_next/static/chunks/app/(protected)/asset/page-21bfa50ff4ec5ee3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8583],{11459:(e,s,r)=>{Promise.resolve().then(r.bind(r,42309))},42309:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>u});var t=r(99888),n=r(72936);function u(){return(0,t.jsx)(n.A,{tab:"market"})}}},e=>{e.O(0,[4292,3054,2487,7432,3842,1575,4228,5671,1580,8693,6549,2936,6546,4552,7358],()=>e(e.s=11459)),_N_E=e.O()}]);
